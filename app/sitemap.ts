65d22ed2bb2cc6d6d836de37cf9f284b639ec7bf
FullConciergerie <contact@full-nevers-conciergerie.fr>
2026-09-14 23:23:13 +0200

    Ajout de /application au plan du site

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -36,6 +36,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/application`,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified,
       changeFrequency: 'yearly',
